200a4b51361ba5a444435094dd64a890a9f05612
Obai Basher <53649412+ObaiBasheer@users.noreply.github.com>
2024-06-02 02:55:59 +0300

    Add Salary list Validation

---
+++ Models/Validation/SalaryListValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace PayrollManagementSystem.Models.Validation
+{
+    public class SalaryListValidator : AbstractValidator<SalaryList>
+    {
+        public SalaryListValidator()
+        {
+            RuleFor(s => s.EmployeeId).GreaterThan(0).WithMessage("EmployeeId must be greater than 0.");
+
+            RuleFor(s => s.Salary).GreaterThan(0).WithMessage("Salary must be greater than 0.");
+
+            
+        }
+    }
+
+}
