5e8247e893e22f757ae6ad4969221b20b09efcc3
Obai Basher <53649412+ObaiBasheer@users.noreply.github.com>
2024-06-02 07:28:58 +0300

    role based Configration

---
+++ Utility/RoleRequirementHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace PayrollManagementSystem.Utility
+{
+    public class RoleRequirementHandler : AuthorizationHandler<RoleRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, RoleRequirement requirement)
+        {
+            if (context.User.IsInRole(requirement.Role))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Utility/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace PayrollManagementSystem.Utility
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static WebApplicationBuilder AddAppAuthentication(this WebApplicationBuilder builder)
+        {
+            var Secret = builder.Configuration.GetValue<string>("JWT:JwtOptions:key");
+            var Issuer = builder.Configuration.GetValue<string>("JWT:JwtOptions:Issuer");
+            var Audience = builder.Configuration.GetValue<string>("JWT:JwtOptions:Audience");
+
+            var key = Encoding.ASCII.GetBytes(Secret!);
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = Issuer,
+                    ValidAudience = Audience,
+                    ValidateAudience = true
+                };
+            });
+
+            return builder;
+        }
+    }
+}
