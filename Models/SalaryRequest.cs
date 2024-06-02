ca802ee82a7908a3612462588c81f184cfc69350
Obai Basher <53649412+ObaiBasheer@users.noreply.github.com>
2024-06-02 04:31:22 +0300

    Create Seperate Model For Salary Request

--- Models/SalaryRequest.cs
+++ Models/SalaryRequest.cs
@@ -5,16 +5,12 @@ namespace PayrollManagementSystem.Models
     public class SalaryRequest
     {
         public int Id { get; set; }
-        [Required]
-        public int EmployeeId { get; set; }
-        [Range(0, double.MaxValue)]
-        public decimal Amount { get; set; }
-        public bool ApprovedByAccountant { get; set; }
-        public bool ApprovedByManager { get; set; }
-        public Employee? Employee { get; set; }
-        [StringLength(255)]
+        public string? Name { get; set; }
+        public bool IsApprovedByAccountant { get; set; }
+        public bool IsApprovedByManager { get; set; }
+        public List<SalaryRequestItem> Items { get; set; }
         public string? AccountantDocumentPath { get; set; }
-        [StringLength(255)]
         public string? ManagerDocumentPath { get; set; }
     }
 }
+
---
+++ Models/SalaryRequestItem.cs
@@ -0,0 +1,10 @@
+﻿namespace PayrollManagementSystem.Models
+{
+    public class SalaryRequestItem
+    {
+        public int Id { get; set; }
+        public int SalaryRequestId { get; set; }
+        public int SalaryId { get; set; }
+        public SalaryList Salary { get; set; }
+    }
+}
